d33e48adc4940ad5ef5e4171732153e925e94174
bellonet <bellonet@gmail.com>
2014-11-01 19:35:13 -0400

    fixed contact link

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,6 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new', as: "login"
   get '/logout', to: 'sessions#destroy', as: "logout"
 
-  get 'contact/index'
-
   get 'about/index'
 
   get 'home/index'
