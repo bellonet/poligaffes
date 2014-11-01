1322c3afc52d3e7b21ecfb4799d2b8a06e6e76ab
bellonet <bellonet@gmail.com>
2014-11-01 18:34:47 -0400

    added index, add, edit, destroy to users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,12 @@
 class UsersController < ApplicationController
+
+	before_filter :authorize, only: [:index, :new, :edit, :destroy]
+
+	
+	def index 
+		@users = User.all
+	end
+
 	def new
 		@user = User.new
 	end
@@ -6,11 +14,32 @@ class UsersController < ApplicationController
 	def create
 		@user = User.new(user_params)
 		if @user.save
-			session[:user_id] = @user.id
-			redirect_to root_url, :notice => "Signed up!"
+			redirect_to users_path, :notice => "Signed up!"
 		else
 			render "new"
 		end
+	end
+
+	def edit
+		 @user = User.find(params[:id])
+	end
+
+	def update
+		@user = User.find(params[:id])
+ 
+  	if @user.update(user_params)
+    	redirect_to users_path
+  	else
+    	render 'edit'
+  	end
+	end
+
+	def destroy
+		@user = User.find(params[:id])
+		session[:user_id] = nil
+  	@user.destroy
+ 
+  	redirect_to users_path
 	end
 
 	def user_params
