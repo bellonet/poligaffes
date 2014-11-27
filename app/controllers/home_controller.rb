86a4448f5a54413d74a93fea1b8a1627cd999dad
bellonet <bellonet@gmail.com>
2014-11-28 01:55:03 +0200

    more hebrew adjustments

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,7 @@ class HomeController < ApplicationController
   end
 
   def search
-  	@yairs = Yair.where("last_name LIKE ?", "%#{params[:search]}%")
+  	@yairs = Yair.where("last_name LIKE ?", "%#{params[:search]}%").paginate(page: params[:page], per_page: 10)
   	render :index
   end
   
