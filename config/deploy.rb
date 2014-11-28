8f68a63bc4a045e033ce71b3038379733ab7cca3
Jonathan Ronen <yablee@gmail.com>
2014-11-28 17:07:36 -0500

    Link database.yml (capistraon)

--- config/deploy.rb
+++ config/deploy.rb
@@ -18,6 +18,7 @@ set :rbenv_ruby, '2.1.5'
 set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
 set :rbenv_map_bins, %w{rake gem bundle ruby rails}
 set :rbenv_roles, :all # default value
+set :linked_files, %w{config/database.yml}
 
 # Default value for :scm is :git
 # set :scm, :git
