0e85e4319c04e50e43ddf11399cbb0d298c67092
bellonet <bellonet@gmail.com>
2014-12-07 12:58:47 +0200

    fixed rep link in admin

--- scripts/fetch_latest_posts.rb
+++ scripts/fetch_latest_posts.rb
@@ -2,9 +2,17 @@
 #!/usr/bin/env ruby
 
 require 'koala'
-api_key = File.read("#{Rails.root}/config/facebook_api_key.txt").strip
+#api_key = File.read("#{Rails.root}/config/facebook_api_key.txt").strip
+api_key = 'CAACEdEose0cBAGExnpeV9q5ZB1ncxqKOCZCospkafQQZCFbZAZCZBqrTnnj7KCQR7tumVnal4Dkth5QvLXLhYTrI2ggeCsCzzzymZB6hEZBB0PIhZCKm5U7rGT7WspaAaO91ZChRkdLAZCWwXg2oNKftLTsmJosnyugaZAm1aqmayuOYeImPWMjSyyIzeYqamOnqy9AndtHH5esbjZBhYFOlIBGjPpFVZAQeZCZCT04ZD'
 g = Koala::Facebook::API.new(api_key)
 
+
+# token = FbApiToken.order(expires: :desc).first
+# if token.expires < DateTime.now
+# 	raise "Invalid access token, enter a new one in /admin/fb_api_tokens"
+# end
+
+#g = Koala::Facebook::API.new(token.token)
 facebook_id = 'yairlapid'
 
 posts = g.get_connections(facebook_id, 'posts')
