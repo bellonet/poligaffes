64e1cef9a49e8e734b0e4756e971fd310e627a37
Jonathan Ronen <yablee@gmail.com>
2014-12-11 15:29:49 -0500

    Remove comment todo

--- scripts/fetch_latest_posts.rb
+++ scripts/fetch_latest_posts.rb
@@ -26,10 +26,5 @@ SocialMediaAccount.where(site: 'Facebook').each do |acc|
                    social_media_account: acc)
     $stdout.write '.'
   end
-  # TODO also, in case all these posts were newer than what's in DB, we need to go to the next page and also save those
-  # posts = posts.next_page
-  # and when you hit "next_page (which chronologically, is "previous" i.e. older posts)
-  # it's not sure that facebook will respect your "since" parameter, so we'll need to
-  # check this one here manually.
   $stdout.write "\n"
 end
