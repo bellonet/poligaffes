f15c9af6eb32ed85e654f8ef083dda44d1487b67
Jonathan Ronen <yablee@gmail.com>
2014-12-11 14:26:26 -0500

    Change puts to write to stderr

--- lib/poligaffes/facebook/errors.rb
+++ lib/poligaffes/facebook/errors.rb
@@ -11,7 +11,7 @@ module Poligaffes
             block.call()
             break
           rescue Koala::Facebook::ServerError => e
-            puts "Graph API Error: #{e.inspect}"
+            $stderr.write " Graph API Error: #{e.inspect} "
             #   2: Temporary issue due to downtime - retry the operation after waiting.
             #   4: Temporary issue due to throttling - retry the operation after waiting and examine your API request volume.
             #  17: Temporary issue due to throttling - retry the operation after waiting and examine your API request volume.
@@ -20,7 +20,7 @@ module Poligaffes
               raise e
             end
             sleep 3
-            puts "retrying..."
+            $stderr.write "retrying"
           end
         end
       end
