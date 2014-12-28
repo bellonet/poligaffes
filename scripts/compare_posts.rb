b6453e5ee00111896a1a8dd37b231c48fd30ec1d
bellonet <bellonet@gmail.com>
2014-12-28 23:34:46 +0100

    added deleted and edited tabs to index

--- scripts/compare_posts.rb
+++ scripts/compare_posts.rb
@@ -8,30 +8,31 @@ puts "Got an access token than expires #{token.expires}"
 
 g = Koala::Facebook::API.new(token.token)
 
-acc = SocialMediaAccount.find_by_id('142')
+SocialMediaAccount.where(site: 'Facebook').each do |acc|
 
-latest_raw_posts = acc.raw_posts.order('timestamp desc').limit(10)
-latest_post_datetime = latest_raw_posts.first.timestamp
+	latest_raw_posts = acc.raw_posts.order('timestamp desc').limit(10)
+	latest_post_datetime = latest_raw_posts.first.timestamp
 
-latest_fb_posts = g.get_connections(acc.link, 'posts', until: latest_post_datetime, limit: 10)
+	latest_fb_posts = g.get_connections(acc.link, 'posts', until: latest_post_datetime, limit: 10)
 
-lfp_m  = latest_fb_posts.map { |p| p['message'] }
-lfp_id = latest_fb_posts.map { |p| p['id'] }
+	lfp_m  = latest_fb_posts.map { |p| p['message'] }
+	lfp_id = latest_fb_posts.map { |p| p['id'] }
 
-latest_raw_posts.each do |lrp|
-	unless lfp_m.include? lrp.post['message']
-		if (lrp.posts.any?) && !(lfp_m.include? lrp.posts.last.body)
-			next
+	latest_raw_posts.each do |lrp|
+		unless lfp_m.include? lrp.post['message']
+			if (lrp.posts.any?) && !(lfp_m.include? lrp.posts.last.body)
+				next
+			end
+			unless lfp_id.include? lrp.post['id']
+				status = "deleted"
+			else
+				status = "edited"
+			end
+			Post.create(body: lrp.post['message'], 
+						status: status,
+						created_at: lrp.timestamp,
+						social_media_account: acc,
+						raw_post: lrp)
 		end
-		unless lfp_id.include? lrp.post['id']
-			status = "deleted"
-		else
-			status = "edited"
-		end
-		Post.create(body: lrp.post['message'], 
-					status: status,
-					created_at: lrp.timestamp,
-					social_media_account: acc,
-					raw_post: lrp)
 	end
 end
