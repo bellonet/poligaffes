0adb1c635d7c7f85850898f0e3bb4f59e1c1be8b
Jonathan Ronen <yablee@gmail.com>
2015-01-04 21:22:15 +0100

    Refactor script

--- scripts/compare_posts.rb
+++ scripts/compare_posts.rb
@@ -1,41 +1,48 @@
+require 'set'
 require 'koala'
 
-token = FbApiToken.order(expires: :desc).first
+def post_deleted(acc, raw_post)
+  Post.create(
+    body:                 raw_post.post['message'], 
+    status:               'deleted',
+    created_at:           raw_post.timestamp,
+    social_media_account: acc,
+    raw_post:             raw_post)
+end
+
+def post_edited(acc, raw_post, new_post)
+  return if raw_post.posts.any? && raw_post.posts.last.body == new_post['message']
+
+  Post.create(
+    body:                 new_post['message'],
+    status:               'edited',
+    social_media_account: acc,
+    raw_post:             raw_post)
+end
+
+token = FbApiToken.order(expires: :desc).last
 if token.expires < DateTime.now
-	raise "Invalid access token, enter a new one in /admin/fb_api_tokens"
+  raise "Invalid access token, enter a new one in /admin/fb_api_tokens"
 end
 puts "Got an access token than expires #{token.expires}"
 
 g = Koala::Facebook::API.new(token.token)
 
 SocialMediaAccount.where(site: 'Facebook').each do |acc|
+  latest_raw_posts = acc.raw_posts.order('timestamp desc').limit(10)
+  next unless latest_raw_posts.any?
 
-	latest_raw_posts = acc.raw_posts.order('timestamp desc').limit(10)
-	
-	next unless latest_raw_posts.any?
+  latest_post_datetime = latest_raw_posts.first.timestamp
+  latest_fb_posts = g.get_connections(acc.link, 'posts', until: latest_post_datetime, limit: 10)
 
-	latest_post_datetime = latest_raw_posts.first.timestamp
+  ids_in_facebook      = Set.new latest_fb_posts.map { |p| p['id'] }
+  facebook_posts       = Hash[latest_fb_posts.map { |p| [p['id'], p] }]
 
-	latest_fb_posts = g.get_connections(acc.link, 'posts', until: latest_post_datetime, limit: 10)
-
-	lfp_m  = latest_fb_posts.map { |p| p['message'] }
-	lfp_id = latest_fb_posts.map { |p| p['id'] }
-
-	latest_raw_posts.each do |lrp|
-		unless lfp_m.include? lrp.post['message']
-			if (lrp.posts.any?) && !(lfp_m.include? lrp.posts.last.body)
-				next
-			end
-			unless lfp_id.include? lrp.post['id']
-				status = "deleted"
-			else
-				status = "edited"
-			end
-			Post.create(body: lrp.post['message'], 
-						status: status,
-						created_at: lrp.timestamp,
-						social_media_account: acc,
-						raw_post: lrp)
-		end
-	end
+  latest_raw_posts.each do |raw_post|
+    if ids_in_facebook.exclude? raw_post.post['id']
+      post_deleted(acc, raw_post)
+    elsif facebook_posts[raw_post.post['id']]['message'] != raw_post.post['message']
+      post_edited(acc, raw_post, facebook_posts[raw_post.post['id']])
+    end
+  end
 end
