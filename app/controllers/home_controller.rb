fca56396a2a6c64ea4c10a7d20b07ac4f7007ac0
bellonet <bellonet@gmail.com>
2015-01-15 14:33:20 -0500

    added will paginate to posts on index

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-  	@posts = Post.all.order(created_at: :desc).paginate(page: params[:page], per_page: 10)
+  	@posts = Post.all.order(created_at: :desc).paginate(page: params[:page], per_page: 5)
   	@length = 400
   end
 
