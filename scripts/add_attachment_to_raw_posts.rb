ab4fb11d9fb8c2eb0ce84a6c28f5fcf9f886144e
Jonathan Ronen <yablee@gmail.com>
2015-01-15 19:48:32 -0500

    Add some logging to attachment script

--- scripts/add_attachment_to_raw_posts.rb
+++ scripts/add_attachment_to_raw_posts.rb
@@ -2,23 +2,28 @@
 #!/usr/bin/env ruby
 
 require 'paperclip'
+require 'time'
 
-RawPost.where("timestamp >?", 1.minute.ago).
+puts "(#{DateTime.now})fetching attachments."
+RawPost.where("timestamp >?", 5.minutes.ago).
 where('attachment_file_name is null').
 where("post ->> 'object_id' LIKE ?", "%%").each do |rp|
 
-  puts rp.id
+  $stdout.write "#{rp.id} - #{rp.social_media_account.link}"
 
   if rp.post['type'] == "photo"
     link = 'https://graph.facebook.com/' + rp.post['object_id'] + '/picture'
     rp.attachment = URI.parse(link)
 
-  elsif rp.post['type'] == "video" 
+  elsif rp.post['type'] == "video"
     rp.attachment = URI.parse(rp.post['source'])
   end
 
   if rp.save
-    puts "saved"
+    puts "... saved\n"
+  else
+    puts "\n"
   end
 
 end
+puts "finished run (#{DateTime.now})"
