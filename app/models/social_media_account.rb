e15421fdac6c1ea6e22cddf3042b5f2aec658bf7
Jonathan Ronen <yablee@gmail.com>
2015-01-22 00:22:28 -0500

    Parameterize fetch script

--- app/models/social_media_account.rb
+++ app/models/social_media_account.rb
@@ -10,5 +10,6 @@ class SocialMediaAccount < ActiveRecord::Base
   has_attached_file :photo, :styles => { :thumb => "100x100#" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :photo, :content_type => /\Aimage\/.*\Z/
 
-  scope :tracking, -> { where(track: true) }
+  scope :tracking,     -> { where(track: true) }
+  scope :non_tracking, -> { where("track IS NULL or track = ?",false) }
 end
