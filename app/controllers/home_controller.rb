fb7b2c5f65b2832bbfc695b326299d6795c8e0c5
bellonet <bellonet@gmail.com>
2015-01-29 03:42:54 -0500

    layout nav-pills deleted edited + missing photo

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,37 +1,11 @@
 class HomeController < ApplicationController
 
-  STATS_SQL = """
-  SELECT date_trunc('day',created_at) AS day, status, count(*)
-  FROM posts WHERE created_at > '%s'
-  GROUP BY status, date_trunc('day',created_at)
-  ORDER BY date_trunc('day', created_at);
-  """
-
   def index
   	@deleted_posts = Post.all.order(created_at: :desc).where(status: "deleted").paginate(page: params[:deleted_page], per_page: 5)
     @edited_posts = Post.all.order(created_at: :desc).where(status: "edited").paginate(page: params[:edited_page], per_page: 5)
 
   	@length = 400
-
-    res = ActiveRecord::Base::connection.execute(STATS_SQL % 1.week.ago.strftime('%Y-%m-%d'))
-    @stats = [[],[]]
-    res.each do |row|
-      date = Date.parse(row['day'])
-      if row['status'] == 'deleted'
-        @stats[0] << {
-          'day' => (DateTime.now - date).to_i,
-          'count' => row['count'].to_i,
-          'name' => 'אבגדהוש'[date.wday]
-          }
-        else
-          @stats[1] << {
-          'day' => (DateTime.now - date).to_i,
-          'count' => row['count'].to_i,
-          'name' => 'אבגדהוש'[date.wday]
-          }
-        end
-      end
-    end
+  end
 
   def search
   	@yairs = Yair.where("last_name LIKE ?", "%#{params[:search]}%").paginate(page: params[:page], per_page: 10)
--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,4 +1,31 @@
 class StatsController < ApplicationController
+
+	STATS_SQL = """
+  SELECT date_trunc('day',created_at) AS day, status, count(*)
+  FROM posts WHERE created_at > '%s'
+  GROUP BY status, date_trunc('day',created_at)
+  ORDER BY date_trunc('day', created_at);
+  """
+
   def index
+  	res = ActiveRecord::Base::connection.execute(STATS_SQL % 1.week.ago.strftime('%Y-%m-%d'))
+    @stats = [[],[]]
+    res.each do |row|
+      date = Date.parse(row['day'])
+      if row['status'] == 'deleted'
+        @stats[0] << {
+          'day' => (DateTime.now - date).to_i,
+          'count' => row['count'].to_i,
+          'name' => 'אבגדהוש'[date.wday]
+          }
+        else
+          @stats[1] << {
+          'day' => (DateTime.now - date).to_i,
+          'count' => row['count'].to_i,
+          'name' => 'אבגדהוש'[date.wday]
+          }
+      end
+    end
   end
+
 end
