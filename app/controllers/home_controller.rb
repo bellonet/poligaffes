0eba1ab7db561fab065d2ed9ae50a28e1b4f7749
bellonet <bellonet@gmail.com>
2015-01-31 03:25:47 -0500

    added latest raw posts to home index

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,12 +3,13 @@ class HomeController < ApplicationController
   def index
   	@deleted_posts = Post.all.order(created_at: :desc).where(status: "deleted").paginate(page: params[:deleted_page], per_page: 5)
     @edited_posts = Post.all.order(created_at: :desc).where(status: "edited").paginate(page: params[:edited_page], per_page: 5)
+    @latest_raw_posts = RawPost.all.order(created_at: :desc).limit(10)
 
   	@length = 420
   end
 
   def search
-  	@yairs = Yair.where("last_name LIKE ?", "%#{params[:search]}%").paginate(page: params[:page], per_page: 10)
+  	@yairs = Yair.where("last_name LIKE ?", "%#{params[:search]}%").paginate(page: params[:page], per_page: 20)
   	render :index
   end
   
