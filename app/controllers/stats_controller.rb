d2f7a1370a1cdd2d7f14620a963599ef8decd376
Jonathan Ronen <yablee@gmail.com>
2015-02-01 23:57:21 -0500

    New weekly stats graph

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -3,10 +3,11 @@ class StatsController < ApplicationController
 
 
   def index
-    @weekly_activity = Rails.cache.fetch('weekly_activity-cache-key') { Poligaffes::Stats.weekly_activity_graph }
-    @weekly_top =  Rails.cache.fetch('weekly_top-cache-key') { Hash[['posts', 'deleted', 'edited'].map { |k| [k, Poligaffes::Stats.top_posts(k, 7)] }] }
+    @weekly_top =  Rails.cache.fetch('weekly_top-cache-key')  { Hash[['posts', 'deleted', 'edited'].map { |k| [k, Poligaffes::Stats.top_posts(k, 7)] }] }
     @monthly_top = Rails.cache.fetch('monthly_top-cache-key') { Hash[['posts', 'deleted', 'edited'].map { |k| [k, Poligaffes::Stats.top_posts(k, 31)] }] }
     @alltime_top = Rails.cache.fetch('alltime_top-cache-key') { Hash[['posts', 'deleted', 'edited'].map { |k| [k, Poligaffes::Stats.top_posts(k, 1e4)] }] }
+
+    @bar_graph = Rails.cache.fetch('weekly_activity-cache-key') { Poligaffes::Stats.weekly_activity_bar }
   end
 
 end
--- lib/poligaffes/stats/polistats.rb
+++ lib/poligaffes/stats/polistats.rb
@@ -1,33 +1,34 @@
 module Poligaffes
   module Stats
 
-    STATS_SQL = """
-      SELECT date_trunc('day',created_at) AS day, status, count(*)
-      FROM posts WHERE created_at > '%s'
-      GROUP BY status, date_trunc('day',created_at)
-      ORDER BY date_trunc('day', created_at);
+    GRAPH_SQL = """ SELECT date_trunc('day',created_at) AS day, status, count(*)
+                    FROM posts WHERE created_at > '%s'
+                    GROUP BY status, date_trunc('day',created_at)
+                    UNION
+                    SELECT date_trunc('day',created_at) AS day, 'total', count(*)
+                    FROM raw_posts
+                    WHERE created_at > '%s'
+                    GROUP BY 1
+                    ORDER BY 1;
       """
-      def self.weekly_activity_graph
-        res = ActiveRecord::Base::connection.execute(STATS_SQL % 1.week.ago.strftime('%Y-%m-%d'))
-        ret = [[],[]]
-        res.each do |row|
+
+      def self.weekly_activity_bar
+        res = ActiveRecord::Base::connection.execute(GRAPH_SQL % [ 1.week.ago.strftime('%Y-%m-%d'), 1.week.ago.strftime('%Y-%m-%d') ])
+        data = res.reduce(Hash.new) do |r, row|
           date = Date.parse(row['day'])
-          if row['status'] == 'deleted'
-            ret[0] << {
-              'day' => (DateTime.now - date).to_i,
-              'count' => row['count'].to_i,
-              'name' => 'אבגדהוש'[date.wday]
-              }
-            else
-              ret[1] << {
-              'day' => (DateTime.now - date).to_i,
-              'count' => row['count'].to_i,
-              'name' => 'אבגדהוש'[date.wday]
-              }
-          end
+          status = {
+            'deleted' => 'deletes',
+            'edited'  => 'edits',
+            'total'   => 'raw_posts'
+          }[row['status']]
+          r[row['day']] ||= Hash.new
+          r[row['day']]['day'] = date
+          r[row['day']]['name'] =  'אבגדהוש'[date.wday]
+          r[row['day']][I18n.t("stats.#{status}")] = row['count'].to_i
+          r
         end
-        ret
-      end # def self.weekly_activity_graph
+        ret = data.keys.sort.reverse.map { |d| data[d] }
+      end # def self.weekly_activity_bar
 
       TOP_POSTS_SQL = "SELECT yairs.id, yairs.first_name || ' ' || yairs.last_name AS name, count(*)
                        FROM posts
