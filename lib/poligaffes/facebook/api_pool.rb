2554d592bbfe080a32f631560c22cba653e15ecd
Jonathan Ronen <yablee@gmail.com>
2015-02-13 07:47:56 -0500

    Slightly better API pool

--- lib/poligaffes/facebook/api_pool.rb
+++ lib/poligaffes/facebook/api_pool.rb
@@ -6,17 +6,27 @@ module Poligaffes
 
     class ApiPool
       def initialize(*graphs)
-        @apis = graphs.map { |g| [g, DateTime.new(0)] }
+        @apis = graphs.map { |g| [g, 'ok'] }
       end
 
       def method_missing(sym, *args, &block)
-        @apis.each do |api|
+        @apis.count.times do |i|
+          api = get_nothrottle_api()
           begin
-            return api[0].__send__ sym, *args, &block
+            return api.__send__ sym, *args, &block
           rescue Koala::Facebook::ClientError => e
+            api[1] = 'throttled'
             $stderr.write " Graph API Error: #{e.inspect} "
             next
           end
+        end
+      end
+
+      private
+
+      def get_nothrottle_api
+        @apis.each do |api|
+          return api[0] if api[1] == 'ok'
         end
       end
     end
