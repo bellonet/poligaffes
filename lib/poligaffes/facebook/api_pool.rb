34793e2fb9031e8a4a40310e8da3455c6fd4f316
Jonathan Ronen <yablee@gmail.com>
2015-02-13 11:03:45 -0500

    Check errors in api pool

--- lib/poligaffes/facebook/api_pool.rb
+++ lib/poligaffes/facebook/api_pool.rb
@@ -15,11 +15,16 @@ module Poligaffes
           begin
             return api[0].__send__ sym, *args, &block
           rescue Koala::Facebook::ClientError => e
-            api[1] = 'throttled'
-            $stderr.write " Graph API Error: #{e.inspect} "
-            next
+            if [17, 613].include? e.fb_error_code # API RATE LIMIT
+              api[1] = 'throttled'
+              $stderr.write " Graph API Error: #{e.inspect} "
+              next
+            else
+              raise e
+            end
           end
         end
+        raise "Poligaffes::Facebook::ApiPool Ran out of APIs in pool... "
       end
 
       private
