f07cdb98f5445ad413fa6891e4068f38d6b466a6
Jonathan Ronen <yablee@gmail.com>
2015-02-15 04:53:58 -0500

    Use own jquery (no rails)

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,7 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require jquery_ujs
-//= require turbolinks
+//= require jquery-2.1.3
 //= require rbootstrap
 //= require d3.min
 //= require_tree .
